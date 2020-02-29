71f54d14125990a79f290386e67b64a4faa28329
Ali Taheri Nastooh <ali.taherinastooh@gmail.com>
2020-02-29 13:10:55 +0330

    complete view of homepage

--- stProject/backend/StockProject/urls.py
+++ stProject/backend/StockProject/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('BackendApp.urls', namespace='posts')),
+    url(r'^$', views.homepage),
 ]
---
+++ stProject/backend/StockProject/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def homepage(request):
+    return render(request,'view.html')
